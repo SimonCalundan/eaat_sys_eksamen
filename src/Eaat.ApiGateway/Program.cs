bb4c5bdfe38bcb5907ff2dada906e70d50da69de
skc <skc@scopito.com>
2026-05-18 21:34:13 +0200

    feat: add gateway

--- src/Eaat.ApiGateway/Program.cs
+++ src/Eaat.ApiGateway/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapReverseProxy();
+
+app.MapGet("/", () => "Eaat API Gateway — see /orders, /restaurant/orders, /deliveries");
 
 app.Run();
