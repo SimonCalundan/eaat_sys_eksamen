c67bdccad5d76c40892452f5ef9400d4e1d178c0
skc <skc@scopito.com>
2026-05-18 21:35:53 +0200

    feat: add db create migration

---
+++ infra/mysql-init/01-create-databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS eaat_orders CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS eaat_restaurants CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS eaat_deliveries CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS eaat_notifications CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
